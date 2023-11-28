0ce4d0855a4a82382462b899d127434e68a66a87
jc-u <jcurbain.admin@gmail.com>
2023-11-28 15:42:06 +0100

    request pyment route moddified

--- routes/payment.js
+++ routes/payment.js
@@ -1,17 +1,21 @@
 const express = require("express");
+const { create } = require("../models/User");
 const router = express.Router();
-const stripe = require("stripe")(process.env.STRIPE_API_SECRET);
+const stripe = require("stripe");
+
+const stripe = createStripe(process.env.STRIPE_API_SECRET);
 
 router.post("/payment", async (req, res) => {
+	console.log(req.body);
 	try {
-		console.log(req.body);
 		// Je récupère le token reçu depuis le front
 		const stripeToken = req.body.stripeToken;
 		// Je fais une requête à stripe pour créer une transaction
 		const responseFromStripe = await stripe.charges.create({
-			token: stripeToken,
-			title: req.body.title,
-			amount: req.body.price * 100,
+			amount: (req.body.amount * 100).toFixed(0),
+			currency: "eur",
+			description: `Paiement vinted pour : ${req.body.title}`,
+			source: req.body.token,
 		});
 		// Si le paiement s'est bien passé, on met à jour l'offre et on renvoie au front le fait que tout s'est bien passé
 
