bac5babfc135ab3639269e3d16e83e2931a05635
jc-u <jcurbain.admin@gmail.com>
2023-11-28 15:44:19 +0100

    request pyment route moddified

--- routes/payment.js
+++ routes/payment.js
@@ -1,27 +1,26 @@
 const express = require("express");
-const { create } = require("../models/User");
 const router = express.Router();
-const stripe = require("stripe");
+const createStripe = require("stripe");
 
+/* Votre clé privée doit être indiquée ici */
 const stripe = createStripe(process.env.STRIPE_API_SECRET);
 
 router.post("/payment", async (req, res) => {
 	console.log(req.body);
 	try {
-		// Je récupère le token reçu depuis le front
-		const stripeToken = req.body.stripeToken;
-		// Je fais une requête à stripe pour créer une transaction
-		const responseFromStripe = await stripe.charges.create({
+		// on envoie le token a Stripe avec le montant
+		let { status } = await stripe.charges.create({
 			amount: (req.body.amount * 100).toFixed(0),
 			currency: "eur",
 			description: `Paiement vinted pour : ${req.body.title}`,
 			source: req.body.token,
 		});
-		// Si le paiement s'est bien passé, on met à jour l'offre et on renvoie au front le fait que tout s'est bien passé
-
-		console.log(responseFromStripe);
-		res.json({ status: responseFromStripe.status });
+		// Le paiement a fonctionné
+		// On peut mettre à jour la base de données
+		// On renvoie une réponse au client pour afficher un message de statut
+		res.json({ status });
 	} catch (error) {
+		console.log(error.message);
 		res.status(500).json({ message: error.message });
 	}
 });
